07a624019972d678104b5549f43d4351c52a309d
MagicAedo <71152030+MagicAedo@users.noreply.github.com>
2021-10-19 17:56:15 -0500

    Se hace la modificacion del views.py

--- prueba/books/views.py
+++ prueba/books/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 from django.template import loader 
 # Create your views here.
 def index(request): 
+    nombre = nombres.objects.all()
     template = loader.get_template('books/index.html')
-    context = {}
+    context = {
+        'nombre': nombre,
+    }
     return HttpResponse(template.render(context, request))
 
 
